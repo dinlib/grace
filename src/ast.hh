9059881fad5c34886725bc0f7e72ca630409f4d7
Guilherme Souza <ogrsouza@gmail.com>
2018-09-06 19:37:47 -0300

    Parsing a simplified function decl

--- src/ast.hh
+++ src/ast.hh
@@ -4,6 +4,10 @@
 #include <string>
 #include <vector>
 
+class StmtNode;
+
+typedef std::vector<StmtNode *> StmtList;
+
 class Node {
 public:
   Node() {}
@@ -11,17 +15,24 @@ public:
   virtual void PrintAST() const {}
 };
 
+class StmtNode : public Node {
+public:
+  void PrintAST() const {}
+};
+
+class ExprNode : public Node {};
+
 class BlockNode : public Node {
 public:
-  std::vector<Node *> nodes;
+  StmtList stmts;
   BlockNode() {}
   virtual void PrintAST() const {
-    for (const auto &node : nodes)
-      node->PrintAST();
+    for (auto &stmt : stmts)
+      stmt->PrintAST();
   }
 };
 
-class VarDeclNode : public Node {
+class VarDeclNode : public StmtNode {
   std::string id;
   unsigned size;
   std::string type;
@@ -31,9 +42,29 @@ public:
       : id(id), size(size), type(type) {}
 
   virtual void PrintAST() const {
-    std::cout << "variable declaration with name: " << id << std::endl
+    std::cout << "variable: " << id << std::endl
               << "size: " << size << std::endl
               << "type: " << type << std::endl
               << std::endl;
+  }
+};
+
+class FuncDeclNode : public StmtNode {
+  std::string name;
+  std::string returnType;
+  BlockNode *block;
+
+public:
+  FuncDeclNode(std::string &name, std::string &returnType, BlockNode *block)
+      : name(name), returnType(returnType), block(block) {}
+
+  void PrintAST() const {
+    std::cout << "function: " << name << std::endl
+              << "return type: " << returnType << std::endl
+              << "block: " << std::endl;
+
+    block->PrintAST();
+
+    std::cout << std::endl;
   }
 };
